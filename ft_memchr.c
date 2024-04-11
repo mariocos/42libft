4cd00fc474ef4868dd2254e9a7b250cf130ec483
Mario Costa De Souza <mariocos@c1r14s4.42lisboa.com>
2024-04-11 14:59:45 +0100

    adding a change in the cast of memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 11:40:23 by mario             #+#    #+#             */
-/*   Updated: 2024/04/10 21:10:28 by mariocos         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:59:34 by mariocos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (*(char *)s == c)
-			return ((char *)s);
+		if (*(unsigned char *)s == c)
+			return ((unsigned char *)s);
 		s++;
 		i++;
 	}
