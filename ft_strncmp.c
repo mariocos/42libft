f733d1983f68957ae2def6c10f0c106014d25686
Mario Costa De Souza <mariocos@c1r14s4.42lisboa.com>
2024-04-11 13:54:51 +0100

    more mods to strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 11:35:26 by mario             #+#    #+#             */
-/*   Updated: 2024/04/11 13:47:37 by mariocos         ###   ########.fr       */
+/*   Updated: 2024/04/11 13:54:30 by mariocos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	ret = 0;
 	i = 0;
+	if (n == 0)
+		return (0);
 	if (*s2 == '\0')
 		ret = s1[i];
 	while (ret == 0 && s1[i] != 0 && i < n)
