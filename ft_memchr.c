ab977661eec4e988c5c5a51533e5a5ca1ee3d27d
Mario Costa De Souza <mariocos@c1r14s4.42lisboa.com>
2024-04-11 15:08:12 +0100

    changes to memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 11:40:23 by mario             #+#    #+#             */
-/*   Updated: 2024/04/11 14:59:34 by mariocos         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:08:00 by mariocos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (n == 0)
+		return (NULL);
+	if (c == 0)
+		return (s + ft_strlen((char const *)s));
 	while (i < n)
 	{
 		if (*(unsigned char *)s == c)
-			return ((unsigned char *)s);
+			return (s);
 		s++;
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
