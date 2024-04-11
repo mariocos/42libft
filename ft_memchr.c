19e2f62e571d292193f01d47cac9333502062c1f
Mario Costa De Souza <mariocos@c1r14s4.42lisboa.com>
2024-04-11 15:09:13 +0100

    forgot to add header to memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,11 +6,12 @@
 /*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 11:40:23 by mario             #+#    #+#             */
-/*   Updated: 2024/04/11 15:08:00 by mariocos         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:09:04 by mariocos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
