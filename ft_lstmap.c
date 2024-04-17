36147632a4165ec9657b9c6c0e7e79a919139153
Mario Costa De Souza <mariocos@c2r12s5.42lisboa.com>
2024-04-17 15:31:37 +0100

    eu esqueci me do asterisco para dizer que era pointer no lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,18 +6,19 @@
 /*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 16:52:23 by mariocos          #+#    #+#             */
-/*   Updated: 2024/04/17 15:14:51 by mariocos         ###   ########.fr       */
+/*   Updated: 2024/04/17 15:29:54 by mariocos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	hold;
-	t_list	s;
+	t_list	*hold;
+	t_list	*s;
 
-	s = 0;
+	s = NULL;
 	while (lst != NULL)
 	{
 		hold = ft_lstnew(f(lst->content));
